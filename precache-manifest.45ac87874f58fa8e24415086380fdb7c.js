af197d7a905de7b24ef6a362d7a7a4c4a46e1b5a
AN-2000 <anirudh.inc.my.company@gmail.com>
2020-08-22 19:21:33 +0530

    Updates

---
+++ precache-manifest.45ac87874f58fa8e24415086380fdb7c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movies/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "17c2eb101911686c894e",
+    "url": "/movies/static/css/main.078b920f.chunk.css"
+  },
+  {
+    "revision": "04129ba559ae92aa086e",
+    "url": "/movies/static/js/runtime~main.04129ba5.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movies/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d2755ea5fe416965f71c",
+    "url": "/movies/static/js/2.d2755ea5.chunk.js"
+  },
+  {
+    "revision": "17c2eb101911686c894e",
+    "url": "/movies/static/js/main.17c2eb10.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movies/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movies/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movies/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "d2755ea5fe416965f71c",
+    "url": "/movies/static/css/2.c4e38c2d.chunk.css"
+  },
+  {
+    "revision": "eeaef02f304f4140dcb256ae0a646536",
+    "url": "/movies/index.html"
+  }
+];
